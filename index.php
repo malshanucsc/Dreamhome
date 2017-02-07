b7b78443c4b5a7f9764c41aa449d415fd14e3304
malshanucsc <randhajapvt@gmail.com>
2017-02-07 22:39:14 +0530

    DreamHome home page complete

---
+++ index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dream Home</title>
+	<link rel="stylesheet" type="text/css" href="Assets/CSS/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="Assets/CSS/mycss.css">
+</head>
+<body>
+
+<div id="upbanner" class="col-md-12" >
+
+	<div class="col-md-2" id="banner-image" >
+	<img  src="Assets/Images/guest.jpeg" alt="..." class="img-responsive">
+		
+	</div>
+	<div id=bannertopic class="col-md-6">
+		
+	<h1>
+		Your Dream Home
+	</h1>
+	</div>
+<br><br><br><br>
+<br>
+
+
+
+</div>
+<div class="col-md-12" id="upmenu">
+<div class="col-md-10 ">
+ <nav class="navbar navbar-default" id="upnav">
+  <ul class="nav navbar-nav">
+    <li id="active"><a href="#">Home</a></li>
+    <li><a href="#">Reviews</a></li>
+    <li><a href="#">Services</a></li>
+    
+    <li><a href="#">Room Rates</a></li>
+    <li><a href="#">Contact</a></li>
+  </ul>
+
+</nav>
+	
+</div>
+	
+</div>
+
+
+
+
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="http://cdn.bootcss.com/animate.css/3.5.1/animate.min.css">
+
+<div class="col-md-12">
+<div id="first-slider" class="col-md-10" >
+    <div id="carousel-example-generic" class="carousel slide carousel-fade">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+        </ol>
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+            <!-- Item 1 -->
+            <div class="item active slide1">
+                <div class="row"><div class="container">
+                    <div class="col-md-3 text-right">
+                      <!--   <img style="max-width: 200px;"  data-animation="animated zoomInLeft" src="http://s20.postimg.org/pfmmo6qj1/window_domain.png"> -->
+                    </div>
+                    <div class="col-md-9 text-left">
+                        <h3 data-animation="animated bounceInDown">Your Home</h3>
+                        <h4 data-animation="animated bounceInUp">Calm environment</h4>             
+                     </div>
+                </div></div>
+             </div> 
+            <!-- Item 2 -->
+            <div class="item slide2">
+                <div class="row"><div class="container">
+                    <div class="col-md-7 text-left">
+                        <h3 data-animation="animated bounceInDown">Your Home</h3>
+                        <h4 data-animation="animated bounceInUp">All the facilities you want </h4>
+                     </div>
+                    <div class="col-md-5 text-right">
+                        <!-- <img style="max-width: 200px;"  data-animation="animated zoomInLeft" src="http://s20.postimg.org/sp11uneml/rack_server_unlock.png"> -->
+                    </div>
+                </div></div>
+            </div>
+            <!-- Item 3 -->
+            <div class="item slide3">
+                <div class="row"><div class="container">
+                    <div class="col-md-7 text-left">
+                        <h3 data-animation="animated bounceInDown">Surfing</h3>
+                        <h4 data-animation="animated bounceInUp">Adventures Surfing</h4>
+                     </div>
+                    <div class="col-md-5 text-right">
+                        <!-- <img style="max-width: 200px;"  data-animation="animated zoomInLeft" src="http://s20.postimg.org/eq8xvxeq5/globe_network.png"> -->
+                    </div>     
+                </div></div>
+            </div>
+            <!-- Item 4 -->
+            <div class="item slide4">
+                <div class="row"><div class="container">
+                    <div class="col-md-7 text-left">
+                        <h3 data-animation="animated bounceInDown">Whale watching</h3>
+                        <h4 data-animation="animated bounceInUp">Get close to the sea</h4>
+                     </div>
+                    <div class="col-md-5 text-right">
+                        <!-- <img style="max-width: 200px;"  data-animation="animated zoomInLeft" src="http://s20.postimg.org/9vf8xngel/internet_speed.png"> -->
+                    </div>  
+                </div></div>
+            </div>
+            <!-- End Item 4 -->
+    
+        </div>
+        <!-- End Wrapper for slides-->
+        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+            <i class="fa fa-angle-left"></i><span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+            <i class="fa fa-angle-right"></i><span class="sr-only">Next</span>
+        </a>
+    </div>
+</div>
+</div>
+
+<div class="pull-left col-md-8">
+	<h2>Vision</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
+<div class="pull-left col-md-8">
+	<h2>Mission</h2>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
+
+
+<div class="col-md-4 pull-right " id="map">
+	            <div class="entry-content full-width-content" style="width:90% !important;">
+                <div id="street-map" style="box-shadow: 0px 0px 3px rgba(0, 0, 0, 0.25); padding: 0; margin: 0 0 30px 0; height: 350px; width: 400px; " >
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d18876.578193580146!2d80.453303895375!3d5.951714497771827!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae13fce3177dc59%3A0xa2d8a78b36dc9c90!2sMirissa!5e0!3m2!1sen!2slk!4v1486484608887" width="400" height="350" frameborder="0" allowfullscreen></iframe></div>
+
+</div>
+</div>
+
+<div id="downbanner" class="col-md-12" >
+
+                  &copy; Cola
+                    
+</div>
+	<script type="text/javascript" src="Assets/JS/jquery.js"></script>
+	<script type="text/javascript" src="Assets/CSS/bootstrap/js/bootstrap.js"></script>
+	<script type="text/javascript" src="Assets/JS/myjs.js"></script>
+</body>
+</html>
